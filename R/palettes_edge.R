b5891a76f847e50400bb94243e76eeddb4637119
luciaedgehealth <122625065+luciaedgehealth@users.noreply.github.com>
2025-05-29 13:09:04 +0100

    New colour functions to ensure consistent colourways. Updated plot function for margins and other tweaks.

---
+++ R/palettes_edge.R
@@ -0,0 +1,92 @@
+# Internal list to store the Edge palettes
+
+.edge_palettes <- list(
+    standard = c("#23203F", "#56BABF", "#F28F64", "#C18DBE", "#559370", "#f0e0fb"),
+    primary = c("#23203F", "#C18DBE", "#f0e0fb", "#575756"), 
+    expanded = c("#23203F", "#56BABF", "#F28F64", "#C18DBE", "#559370", "#878787", "#266164", "#494384", "#A3C736", "#BDBADC", "#6C3B69"),
+    xl = c(
+        "#23203F", "#878787", "#F28F64", "#266164", "#494384", "#559370", "#C18DBE", "maroon", "#A3C736", "#BDBADC", "#6C3B69",
+        "#41C7C4", "#3940CF", "#A54ABE", "#471D53", "#AFBFD5", "#C7B9CB", "#FF7F50", "#008080", "#FFD700", "#8A2BE2", "#FF6347",
+        "#20B2AA", "#8B4513", "#9370DB", "dodgerblue2", "#E31A1C", "green4", "#6A3D9A", "#FF7F00", "black", "steelblue4", "darkorange",
+        "darkgreen", "darkred", "darkblue", "darkviolet", "darkturquoise", "mediumseagreen", "firebrick", "orchid", "slateblue", "goldenrod", "cadetblue", "chocolate"
+    )
+)
+
+#' Edge Color Palette Generator
+#'
+#' This function is used by the \code{scale_colour_edge} and \code{scale_fill_edge}
+#' functions to generate color palettes.
+#'
+#' @param palette Character name of palette in \code{.edge_palettes}.
+#' @param reverse Boolean indicating whether the palette should be reversed.
+#' @param ... Additional arguments to pass to \code{grDevices::colorRampPalette()}.
+#' @return A function that takes an integer \code{n} and returns \code{n} colors.
+#' @keywords internal # This function is not meant to be called directly by users
+#' @importFrom grDevices colorRampPalette
+edge_pal <- function(palette = "standard", reverse = FALSE, ...) {
+if (!palette %in% names(.edge_palettes)) {
+    stop(paste("Palette", palette, "not found. Available palettes:",
+
+paste(names(.edge_palettes), collapse = ", ")))
+}
+pal <- .edge_palettes[[palette]]
+if (reverse) pal <- rev(pal)
+grDevices::colorRampPalette(pal, ...)
+}
+
+#' @title Edge Colour and Fill Scales for ggplot2
+#' @description
+#' These functions provide discrete colour and fill scales based on predefined "edge" palettes.
+#' Use these with ggplot2 to apply your custom color schemes.
+#'
+#' @param palette Character name of the palette to use.
+#'                Available palettes are: \code{"standard"}, \code{"primary"}, \code{"expanded"},\code{"xl"}.
+#' @param reverse Boolean, if \code{TRUE}, the order of colors is reversed.
+#' @param ... Additional arguments passed to \code{ggplot2::discrete_scale()}.
+#'
+#' @return A ggplot2 scale function.
+#' @name scale_edge
+#' @rdname scale_edge
+#'
+#' @examples
+#' \dontrun{
+#' library(ggplot2)
+#'
+#' # Example with theme_edge and edge_core palette
+#' ggplot(mpg, aes(x = class, y = hwy, fill = class)) +
+#'   geom_col() +
+#'   theme_edge() +
+#'   scale_fill_edge(palette = "standard") +
+#'   labs(title = "Highway Mileage by Class", subtitle = "Using standard palette")
+#'
+#' ggplot(iris, aes(x = Sepal.Length, y = Sepal.Width, colour = Species)) +
+#'   geom_point(size = 3) +
+#'   theme_edge() +
+#'   scale_colour_edge(palette = "primary") +
+#'   labs(title = "Iris Dataset", subtitle = "Using primary palette for points")
+#'}
+NULL # Necessary for roxygen2 to process the @name/@rdname block above
+
+#' @rdname scale_edge
+#' @export
+#' @importFrom ggplot2 discrete_scale
+scale_colour_edge <- function(palette = "standard", reverse = FALSE, ...) {
+ggplot2::discrete_scale(
+    aesthetics = "colour",
+    scale_name = paste0("edge_", palette),
+    palette = edge_pal(palette = palette, reverse = reverse),
+    ...
+)
+}
+
+#' @rdname scale_edge
+#' @export
+#' @importFrom ggplot2 discrete_scale # Already imported if in same file and scale_colour_edge is above
+scale_fill_edge <- function(palette = "edge6", reverse = FALSE, ...) {
+ggplot2::discrete_scale(
+    aesthetics = "fill",
+    scale_name = paste0("edge_", palette),
+    palette = edge_pal(palette = palette, reverse = reverse),
+    ...
+)
+}
--- R/theme_edge.R
+++ R/theme_edge.R
@@ -1,49 +1,53 @@
-# Hello, world!
-#
-# This is an example function named 'hello'
-# which prints 'Hello, world!'.
-#
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
-#
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Install Package:           'Ctrl + Shift + B'
-#   Check Package:             'Ctrl + Shift + E'
-#   Test Package:              'Ctrl + Shift + T'
+#' A Custom ggplot2 Theme: theme_edge
+#'
+#' This function applies a custom theme to ggplot2 plots, featuring a
+#' clean and bold aesthetic.
+#'
+#' @param base_size Base font size, given in pts.
+#' @param base_family Base font family.
+#'
+#' @return A ggplot2 theme object.
+#' @export
+#' @importFrom ggplot2 theme_minimal theme element_text element_blank element_rect element_line margin "%+replace%"
+#'
+#' @examples
+#' \dontrun{
+#' library(ggplot2)
+#'
+#' ggplot(mpg, aes(x = displ, y = hwy, color = class)) +
+#'   geom_point() +
+#'   theme_edge() +
+#'   labs(title = "Engine Displacement vs. Highway MPG",
+#'        subtitle = "Styled with theme_edge")
+#' }
 
 theme_edge <- function() {
-
+  
   theme_minimal() %+replace%
-
+    
     theme(
       title = element_text(colour = "#23203F"),
       text = element_text(colour = "#23203F"),
-      plot.title = element_text(size = 20, face = "bold", hjust = 0, vjust = 10),
-      plot.subtitle = element_text(size = 18, hjust = 0, vjust = 9),
-      plot.caption = element_blank(),
-      axis.title.x = element_text(size = 15.5, face = "bold", margin = margin(t = 20, b = 10), hjust = 0.5),
-      axis.title.y = element_text(size = 15.5, angle = 90, face = "bold", margin = margin(l = 25, r = 25)),
-      axis.text.x = element_text(size = 15.5),
-      axis.text.y = element_text(size = 15.5),
+      plot.title = element_text(size = rel(1.8), face = "bold", hjust = 0, margin = margin(b = 20)),
+      plot.subtitle = element_text(size = rel(1.5), hjust = 0, margin = margin(b = 20)),
+      plot.caption = element_text(hjust=1, size=rel(0.8)),
+      axis.title.x = element_text(size = rel(1.3), face = "bold", margin = margin(t = 20, b = 10), hjust = 0.5),
+      axis.title.y = element_text(size = rel(1.3), angle = 90, face = "bold", margin = margin(l = 15, r = 15)),
+      axis.text = element_text(size = rel(1.2)),
       axis.line.y = element_blank(),
       axis.line.x = element_blank(),
       legend.position = "bottom",
       legend.title = element_blank(),
-      legend.text = element_text(size = 15.5),
+      legend.text = element_text(size = rel(1.2)),
       legend.background = element_blank(),
-      legend.margin = margin(10, 10, 20, 10),
+      legend.margin = margin(t = 5, b = 5),
       legend.direction = "horizontal",
-      plot.margin = margin(70, 50, 10, 10),
+      plot.margin = margin(t = 20, r = 20, b = 10, l = 0),
       panel.grid.major.x = element_blank(),
       panel.grid.minor.x = element_blank(),
       panel.grid.minor.y = element_blank(),
-    #  plot.background = element_rect(fill = "#Fff"),
-     # panel.grid.major =  element_line(colour = "#DAE8EE"),
-     # panel.grid.minor =  element_line(colour = "#DAE8EE"),
-      #strip.background = element_blank(),
-      strip.text = element_text(size = 14),
+      strip.text = element_text(size = rel(1.1), face = "bold", colour = "#23203F"),
       panel.border = element_rect(colour = "#23203F", fill=NA, linewidth=1)
     )
 }
+
